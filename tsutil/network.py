2a45fdc38574907393fa409b59977c492c21d9e1
Yan Wong <yan.wong@bdi.ox.ac.uk>
2022-12-08 13:55:39 +0000

    Demo a download function

---
+++ tsutil/network.py
@@ -0,0 +1,49 @@
+# MIT License
+#
+# Copyright (c) 2022 Tskit Developers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+Utilities that involve network access
+"""
+
+import urllib.request
+
+import tqdm
+import tskit
+import tszip
+
+class DownloadProgressBar(tqdm.tqdm):
+    def update_to(self, b=1, bsize=1, tsize=None):
+        if tsize is not None:
+            self.total = tsize
+        self.update(b * bsize - self.n)
+
+def download(url, progress=True):
+    with DownloadProgressBar(
+        unit='B',
+        unit_scale=True,
+        miniters=1,
+        desc=url.split('/')[-1],
+        disable=not progress,
+    ) as t:
+        tmp_fn, _ = urllib.request.urlretrieve(url, reporthook=t.update_to)
+        ts = tszip.decompress(tmp_fn) if url.endswith(".tsz") else tskit.load(tmp_fn)
+        urllib.request.urlcleanup() # Remove tmp_fn
+    return ts
